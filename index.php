50d2960a0dfcc92da4412a48f71dbc84501cc003
SapphireCascade <codeychase64@gmail.com>
2024-04-06 14:04:20 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "vendor/autoload.php";
+    use Dandylion\ProductDetails;
+    include "src/ProductDetails.php";
+    var_dump(ProductDetails::url("https://www.noahfinnce.com/products/i-saw-noah-finnce-black-t-shirt-noa?variant=40818540707929"));
---
+++ src/ProductDetails.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Dandylion;
+    use Curl\Curl;
+    class ProductDetails {
+        public static function url($url){
+            $curl = new Curl();
+            $curl->get($url);
+            $html = $curl->response;
+            if($html){
+                $dom = new \DOMDocument();
+                @$dom->loadHTML($html);
+                $xpath = new \DOMXPath($dom);
+                $title = $xpath->query('//title')->item(0)->nodeValue;
+                $description = $xpath->query('//meta[@name="description"]')->item(0)->getAttribute('content');
+                $image = $xpath->query('//meta[@property="og:image"]')->item(0)->getAttribute('content');
+                $price = $xpath->query('//meta[@property="og:price:amount"]')->item(0)->getAttribute('content');
+                $currency = $xpath->query('//meta[@property="og:price:currency"]')->item(0)->getAttribute('content');
+                $response = [
+                    'title' => $title,
+                    'description' => $description,
+                    'image' => $image,
+                    'price' => $price,
+                    'currency' => $currency
+                ];
+                return $response;
+            } else {
+                return null;
+            }
+        }
+    }
