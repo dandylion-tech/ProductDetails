f2ca5e1b1ef24f7a4dbe742d9253e11fed89a7d0
SapphireCascade <codeychase64@gmail.com>
2024-04-20 16:50:08 +0100

    standardised attribute names

--- src/ProductDetails.php
+++ src/ProductDetails.php
@@ -4,20 +4,26 @@
     class ProductDetails {
         public static function url($url){
             $curl = new Curl();
-            $curl->setUserAgent('ProductDetails/7.36.3');
+            $curl->setUserAgent('ProductDetails/1');
             $curl->get($url);
             $html = $curl->response;
             if($html){
                 $dom = new \DOMDocument();
                 @$dom->loadHTML($html);
                 $xpath = new \DOMXPath($dom);
-                $data = $xpath->query("//meta[starts-with(@property, 'og:')]");
-                //$data = $xpath->query("//meta");
-                $response = [];
-                for($i=0;$i<$data->length;$i++){
-                    $name = str_replace('og:', '', $data->item($i)->getAttribute('property'));
-                    $response[$name] = trim($data->item($i)->getAttribute('content'));
+                $meta_data = $xpath->query("//meta[starts-with(@property, 'og:')]");
+                $data = [];
+                for($i=0;$i<$meta_data->length;$i++){
+                    $name = str_replace('og:', '', $meta_data->item($i)->getAttribute('property'));
+                    $data[$name] = trim($meta_data->item($i)->getAttribute('content'));
                 }
+                $response = [
+                    "title"=>$data["title"],
+                    "link"=>$data["url"],
+                    "currency"=>$data["price:currency"],
+                    "price"=>$data["price:amount"],
+                    "image_url"=>$data["image:secure_url"]??$data["image"]
+                ];
                 return (object)$response;
             } else {
                 return null;
